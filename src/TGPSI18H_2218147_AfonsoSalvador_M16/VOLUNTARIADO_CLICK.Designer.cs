6eaae1294668e752800644ada42bdcb52cbf18e6
Afonso André de Macedo Salvador <2219147@alunos.escoladigital.com>
2020-05-12 00:32:47 +0100

    commit 1.0

--- src/TGPSI18H_2218147_AfonsoSalvador_M16/VOLUNTARIADO_CLICK.Designer.cs
+++ src/TGPSI18H_2218147_AfonsoSalvador_M16/VOLUNTARIADO_CLICK.Designer.cs
@@ -37,7 +37,7 @@
             // 
             this.panel1.Location = new System.Drawing.Point(33, 136);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(839, 181);
+            this.panel1.Size = new System.Drawing.Size(596, 181);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
             // 
